8caf8f4e8dae3506c150695586ce2d12d92475a8
ebelisia <eduardo.belisia@gmail.com>
2021-11-20 14:40:59 -0300

    first commit

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,9 +8,9 @@ INSERT INTO citieye.city(id, name)VALUES(3, 'Minas Gerais');
 INSERT INTO citieye.city(id, name)VALUES(5, 'Espirito Santo');
 
 --USER
-INSERT INTO citieye.user(id, email, name, password, surname)VALUES(1, 'joaodasilva@gmail.com', 'João', 'senha123', 'Da Silva');
-INSERT INTO citieye.user(id, email, name, password, surname)VALUES(2, 'joaquinoliveira@gmail.com', 'Joaquin', 'senha123', 'Oliveira');
-INSERT INTO citieye.user(id, email, name, password, surname)VALUES(3, 'mariarosa@gmail.com', 'Maria', 'senha123', 'Rosa');
+INSERT INTO citieye.user(id, email, name, surname)VALUES(1, 'joaodasilva@gmail.com', 'João', 'Da Silva');
+INSERT INTO citieye.user(id, email, name, surname)VALUES(2, 'joaquinoliveira@gmail.com', 'senha123', 'Oliveira');
+INSERT INTO citieye.user(id, email, name, surname)VALUES(3, 'mariarosa@gmail.com', 'Maria', 'Rosa');
 
 --CATEGORY
 INSERT INTO citieye.category(id, name)VALUES(1, 'Saúde');

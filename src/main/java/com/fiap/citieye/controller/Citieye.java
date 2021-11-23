658f9614dc7362c440e42153df2a2ac810d8f753
ebelisia <eduardo.belisia@gmail.com>
2021-11-23 13:27:45 -0300

    removing CrossOrigin

--- src/main/java/com/fiap/citieye/controller/Citieye.java
+++ src/main/java/com/fiap/citieye/controller/Citieye.java
@@ -20,40 +20,40 @@ import java.util.Optional;
 
 public interface Citieye {
 
-    @CrossOrigin(origins = "http://localhost:3001")
+//    @CrossOrigin(origins = "http://localhost:3001")
     @PostMapping("/occurrences")
     ResponseEntity<OccurrenceDto> registerOccurrence(final @RequestBody Occurrence occurrence,
                                                      final @RequestParam Long userid);
 
-    @CrossOrigin(origins = "http://localhost:3001")
+//    @CrossOrigin(origins = "http://localhost:3001")
     @PostMapping("/occurrences/{id}")
     void deleteOccurrence(final @PathVariable Long occurrenceId);
 
-    @CrossOrigin(origins = "http://localhost:3001")
+//    @CrossOrigin(origins = "http://localhost:3001")
     @GetMapping("/occurrences/all")
     ResponseEntity<List<OccurrenceDto>> showAllOccurrence();
 
-    @CrossOrigin(origins = "http://localhost:3001")
+//    @CrossOrigin(origins = "http://localhost:3001")
     @GetMapping("/occurrences/city/{city}")
     ResponseEntity<List<OccurrenceDto>> showAllOccurrenceByCity(@PathVariable Long city);
 
-    @CrossOrigin(origins = "http://localhost:3001")
+//    @CrossOrigin(origins = "http://localhost:3001")
     @GetMapping("/occurrences/user/{userId}")
     ResponseEntity<List<OccurrenceDto>> showAllOccurrenceByUserId(@PathVariable Long userId);
 
-    @CrossOrigin(origins = "http://localhost:3001")
+//    @CrossOrigin(origins = "http://localhost:3001")
     @PostMapping("/users")
     ResponseEntity<User> registerUser(final @RequestBody User user);
 
-    @CrossOrigin(origins = "http://localhost:3001")
+//    @CrossOrigin(origins = "http://localhost:3001")
     @GetMapping("/users/{userId}")
     ResponseEntity<Optional<User>> getUser(final @PathVariable Long userId);
 
-    @CrossOrigin(origins = "http://localhost:3001")
+//    @CrossOrigin(origins = "http://localhost:3001")
     @GetMapping("/cities")
     ResponseEntity<List<City>> showAllCities();
 
-    @CrossOrigin(origins = "http://localhost:3001")
+//    @CrossOrigin(origins = "http://localhost:3001")
     @GetMapping("/categories")
     ResponseEntity<List<Category>> showAllcategories();
 

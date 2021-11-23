9d8fdf9dec6b50a2c05ece0bea5f37f6d1bb9695
ebelisia <eduardo.belisia@gmail.com>
2021-11-23 15:14:44 -0300

    adding crossOrigin

--- src/main/java/com/fiap/citieye/controller/Citieye.java
+++ src/main/java/com/fiap/citieye/controller/Citieye.java
@@ -20,40 +20,39 @@ import java.util.Optional;
 
 public interface Citieye {
 
-//    @CrossOrigin(origins = "http://localhost:3001")
+    @CrossOrigin(origins = "https://citieye-app.herokuapp.com")
     @PostMapping("/occurrences")
     ResponseEntity<OccurrenceDto> registerOccurrence(final @RequestBody Occurrence occurrence,
                                                      final @RequestParam Long userid);
-
-//    @CrossOrigin(origins = "http://localhost:3001")
+    @CrossOrigin(origins = "https://citieye-app.herokuapp.com")
     @PostMapping("/occurrences/{id}")
     void deleteOccurrence(final @PathVariable Long occurrenceId);
 
-//    @CrossOrigin(origins = "http://localhost:3001")
+    @CrossOrigin(origins = "https://citieye-app.herokuapp.com")
     @GetMapping("/occurrences/all")
     ResponseEntity<List<OccurrenceDto>> showAllOccurrence();
 
-//    @CrossOrigin(origins = "http://localhost:3001")
+    @CrossOrigin(origins = "https://citieye-app.herokuapp.com")
     @GetMapping("/occurrences/city/{city}")
     ResponseEntity<List<OccurrenceDto>> showAllOccurrenceByCity(@PathVariable Long city);
 
-//    @CrossOrigin(origins = "http://localhost:3001")
+    @CrossOrigin(origins = "https://citieye-app.herokuapp.com")
     @GetMapping("/occurrences/user/{userId}")
     ResponseEntity<List<OccurrenceDto>> showAllOccurrenceByUserId(@PathVariable Long userId);
 
-//    @CrossOrigin(origins = "http://localhost:3001")
+    @CrossOrigin(origins = "https://citieye-app.herokuapp.com")
     @PostMapping("/users")
     ResponseEntity<User> registerUser(final @RequestBody User user);
 
-//    @CrossOrigin(origins = "http://localhost:3001")
+    @CrossOrigin(origins = "https://citieye-app.herokuapp.com")
     @GetMapping("/users/{userId}")
     ResponseEntity<Optional<User>> getUser(final @PathVariable Long userId);
 
-//    @CrossOrigin(origins = "http://localhost:3001")
+    @CrossOrigin(origins = "https://citieye-app.herokuapp.com")
     @GetMapping("/cities")
     ResponseEntity<List<City>> showAllCities();
 
-//    @CrossOrigin(origins = "http://localhost:3001")
+    @CrossOrigin(origins = "https://citieye-app.herokuapp.com")
     @GetMapping("/categories")
     ResponseEntity<List<Category>> showAllcategories();
 

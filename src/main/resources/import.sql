0f90f9657ad279bac8297de6493c05b886c6475b
Eduardo Belisia <49886362+ebelisia@users.noreply.github.com>
2021-11-24 22:30:46 -0300

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,31 +2,31 @@
 -- noinspection SqlDialectInspectionForFile
 
 --CITIES
-INSERT INTO heroku_013a36508df5568.city(id, name)VALUES(1, 'São Paulo');
-INSERT INTO heroku_013a36508df5568.city(id, name)VALUES(2, 'Campinas');
-INSERT INTO heroku_013a36508df5568.city(id, name)VALUES(3, 'São José Dos Campos');
-INSERT INTO heroku_013a36508df5568.city(id, name)VALUES(4, 'Santos');
-INSERT INTO heroku_013a36508df5568.city(id, name)VALUES(5, 'Jundiaí');
-INSERT INTO heroku_013a36508df5568.city(id, name)VALUES(6, 'São Bernardo Do Campo');
-INSERT INTO heroku_013a36508df5568.city(id, name)VALUES(7, 'Americana');
-INSERT INTO heroku_013a36508df5568.city(id, name)VALUES(8, 'Sumaré');
-INSERT INTO heroku_013a36508df5568.city(id, name)VALUES(9, 'Limeira');
-INSERT INTO heroku_013a36508df5568.city(id, name)VALUES(10, 'Embu das Artes');
-INSERT INTO heroku_013a36508df5568.city(id, name)VALUES(11, 'Barueri');
-INSERT INTO heroku_013a36508df5568.city(id, name)VALUES(12, 'São Carlos');
-INSERT INTO heroku_013a36508df5568.city(id, name)VALUES(13, 'Cotia');
-INSERT INTO heroku_013a36508df5568.city(id, name)VALUES(14, 'Jandira');
-INSERT INTO heroku_013a36508df5568.city(id, name)VALUES(15, 'Diadema');
-INSERT INTO heroku_013a36508df5568.city(id, name)VALUES(16, 'Mogi das Cruzes');
+INSERT INTO citieye.city(id, name)VALUES(1, 'São Paulo');
+INSERT INTO citieye.city(id, name)VALUES(2, 'Campinas');
+INSERT INTO citieye.city(id, name)VALUES(3, 'São José Dos Campos');
+INSERT INTO citieye.city(id, name)VALUES(4, 'Santos');
+INSERT INTO citieye.city(id, name)VALUES(5, 'Jundiaí');
+INSERT INTO citieye.city(id, name)VALUES(6, 'São Bernardo Do Campo');
+INSERT INTO citieye.city(id, name)VALUES(7, 'Americana');
+INSERT INTO citieye.city(id, name)VALUES(8, 'Sumaré');
+INSERT INTO citieye.city(id, name)VALUES(9, 'Limeira');
+INSERT INTO citieye.city(citieye, name)VALUES(10, 'Embu das Artes');
+INSERT INTO citieye.city(id, name)VALUES(11, 'Barueri');
+INSERT INTO citieye.city(id, name)VALUES(12, 'São Carlos');
+INSERT INTO citieye.city(id, name)VALUES(13, 'Cotia');
+INSERT INTO citieye.city(id, name)VALUES(14, 'Jandira');
+INSERT INTO citieye.city(id, name)VALUES(15, 'Diadema');
+INSERT INTO citieye.city(id, name)VALUES(16, 'Mogi das Cruzes');
 
 --CATEGORY
-INSERT INTO heroku_013a36508df5568.category(id, name)VALUES(1, 'Saúde');
-INSERT INTO heroku_013a36508df5568.category(id, name)VALUES(2, 'Assistencia Social');
-INSERT INTO heroku_013a36508df5568.category(id, name)VALUES(3, 'Transporte/Mobilidade');
-INSERT INTO heroku_013a36508df5568.category(id, name)VALUES(4, 'Elétrica');
-INSERT INTO heroku_013a36508df5568.category(id, name)VALUES(5, 'Sinalização em vias');
-INSERT INTO heroku_013a36508df5568.category(id, name)VALUES(6, 'Obras');
-INSERT INTO heroku_013a36508df5568.category(id, name)VALUES(7, 'Alagamento');
-INSERT INTO heroku_013a36508df5568.category(id, name)VALUES(8, 'Criminalidade');
-INSERT INTO heroku_013a36508df5568.category(id, name)VALUES(9, 'Lazer');
-INSERT INTO heroku_013a36508df5568.category(id, name)VALUES(10, 'Saneamento Básico');
+INSERT INTO citieye.category(id, name)VALUES(1, 'Saúde');
+INSERT INTO citieye.category(id, name)VALUES(2, 'Assistencia Social');
+INSERT INTO citieye.category(id, name)VALUES(3, 'Transporte/Mobilidade');
+INSERT INTO citieye.category(id, name)VALUES(4, 'Elétrica');
+INSERT INTO citieye.category(id, name)VALUES(5, 'Sinalização em vias');
+INSERT INTO citieye.category(id, name)VALUES(6, 'Obras');
+INSERT INTO citieye.category(id, name)VALUES(7, 'Alagamento');
+INSERT INTO citieye.category(id, name)VALUES(8, 'Criminalidade');
+INSERT INTO citieye.category(id, name)VALUES(9, 'Lazer');
+INSERT INTO citieye.category(id, name)VALUES(10, 'Saneamento Básico');
